9f9b59a7fd101ee311a96bb36cebf559d6a68642
Adrien THIERRY <adrien.thierryfr@gmail.com>
2019-01-17 17:05:50 +0000

    Updated

--- discord/data/resources/electron/browser/api/auto-updater/auto-updater-native.js
+++
@@ -1,8 +0,0 @@
-const EventEmitter = require('events').EventEmitter
-const {autoUpdater, AutoUpdater} = process.atomBinding('auto_updater')
-
-// AutoUpdater is an EventEmitter.
-Object.setPrototypeOf(AutoUpdater.prototype, EventEmitter.prototype)
-EventEmitter.call(autoUpdater)
-
-module.exports = autoUpdater
--- discord/data/resources/electron/browser/api/screen.js
+++
@@ -1,8 +0,0 @@
-const {EventEmitter} = require('events')
-const {screen, Screen} = process.atomBinding('screen')
-
-// Screen is an EventEmitter.
-Object.setPrototypeOf(Screen.prototype, EventEmitter.prototype)
-EventEmitter.call(screen)
-
-module.exports = screen
